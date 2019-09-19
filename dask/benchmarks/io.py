e6c86ab5dc32918eddffc3aaf123eee7a18d432f
Tom Augspurger <tom.w.augspurger@gmail.com>
2019-09-19 13:44:47 -0500

    Fixups

--- dask/benchmarks/io.py
+++ dask/benchmarks/io.py
@@ -4,8 +4,6 @@ import string
 
 import numpy as np
 import pandas as pd
-import dask
-import dask.multiprocessing
 import dask.dataframe as dd
 
 from .common import DaskSuite, rnd
@@ -27,7 +25,7 @@ def mkdf(rows=1000, files=50, n_floats=4, n_ints=4, n_strs=4):
 
 
 class CSV(DaskSuite):
-    params = [dask.get, dask.multiprocessing.get, dask.threaded.get]
+    params = ['single-threaded', 'processes', 'threads']
     data_dir = 'benchmark_data'
     n_files = 30
 
@@ -42,15 +40,17 @@ class CSV(DaskSuite):
     def teardown_cache(self):
         shutil.rmtree(self.data_dir)
 
-    def time_read_csv_meta(self, get):
+    def time_read_csv_meta(self, scheduler):
         return dd.read_csv('{}/*.csv'.format(self.data_dir))
 
-    def time_read_csv(self, get):
-        return dd.read_csv('{}/*.csv'.format(self.data_dir)).compute(get=get)
+    def time_read_csv(self, scheduler):
+        return dd.read_csv('{}/*.csv'.format(self.data_dir)).compute(
+            scheduler=scheduler
+        )
 
 
 class HDF5(DaskSuite):
-    params = [dask.get, dask.multiprocessing.get, dask.threaded.get]
+    params = ['single-threaded', 'processes', 'threads']
     data_dir = 'benchmark_data'
     n_files = 10
 
@@ -64,16 +64,16 @@ class HDF5(DaskSuite):
             df.to_hdf('{}/{}.hdf5'.format(self.data_dir, i), 'key',
                       format='table')
 
-    def setup(self, get):
-        if get is dask.multiprocessing.get:
+    def setup(self, scheduler):
+        if scheduler == 'processes':
             raise NotImplementedError()
 
     def teardown_cache(self):
         shutil.rmtree(self.data_dir)
 
-    def time_read_hdf5_meta(self, get):
+    def time_read_hdf5_meta(self, scheduler):
         dd.read_hdf('{}/*.hdf5'.format(self.data_dir), 'key')
 
-    def time_read_hdf5(self, get):
+    def time_read_hdf5(self, scheduler):
         (dd.read_hdf('{}/*.hdf5'.format(self.data_dir), 'key')
-           .compute(get=get))
+           .compute(scheduler=scheduler))
